cb4a989d37ff34e3eba37af6e99e75f1801bea99
KenKuffler <kennethrkuffler@hotmail.com>
2024-12-03 19:38:14 -0600

    seed.ts changes 2

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -3,6 +3,11 @@ import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve the current directory in ESModule
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const questionsPath = path.resolve(__dirname, "./pythonQuestions.json");
 
@@ -17,14 +22,11 @@ db.once("open", async () => {
 
     console.log("Questions seeded successfully!");
   } catch (error) {
-    if (error instanceof Error) {
-      console.error("Error seeding questions:", error.message);
-    } else {
-      console.error("Error seeding questions:", error);
-    }
+    console.error("Error seeding questions:", error.message);
     process.exit(1);
   } finally {
     process.exit(0);
   }
 });
+
 
