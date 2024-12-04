d92531180c683b8d670bdb57fb0fda8094d1c66a
KenKuffler <kennethrkuffler@hotmail.com>
2024-12-03 20:28:37 -0600

    server.ts and index.ts changes(deploy 5)

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,15 +22,6 @@ app.use(express.json());
 // API routes
 app.use('/api', routes);
 
-// Catch-all route to serve React's index.html
-app.get('*', (req, res) => {
-   if (req.path.startsWith('/api')) {
-      res.status(404).json({ error: 'API route not found' });
-   } else {
-      res.sendFile(path.join(clientDistPath, 'index.html'));
-   }
-});
-
 // Start the server once the database connection is open
 db.once('open', () => {
    app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
